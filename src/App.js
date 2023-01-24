e62f908363e53e87bc4c11eb27b928f32b9a9562
naziya <naaziyapatel36@gmail.com>
2023-01-24 22:14:31 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import PostView from './components/PostView/PostView';
+import FormPage from './components/Form/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route index element={ <LandingPage/> } />
+      <Route path='/Postview' element={ <PostView/> } />
+      <Route  path='FormPage' element={ <FormPage/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
